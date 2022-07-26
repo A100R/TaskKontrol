1cfd4764d5a3839f8a6cf0e0b90989ad1bbe9c77
IrAskhat <106676539+IrAskhat@users.noreply.github.com>
2022-07-26 20:23:15 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] mass = new string[4] {"hello", "2", "world", ":-)"};
+string[] array = new string[mass.Length];
+void ArrayStrings(string[] mass, string[] array)
+{
+    int n = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+      if(mass[i].Length <= 3)
+        {
+        array[n] = mass[i];
+        n++;
+        }
+    }
+}
+void PrintArray(string[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayStrings(mass, array);
+PrintArray(array);
